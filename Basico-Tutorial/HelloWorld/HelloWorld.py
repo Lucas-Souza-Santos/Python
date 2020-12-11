f65203c0f04405aafd49296690bd88ae5e655a44
Lucas-Souza-Santos <lucas210souza@gmail.com>
2020-12-10 22:35:31 -0300

    first commit

---
+++ Basico-Tutorial/HelloWorld/HelloWorld.py
@@ -0,0 +1,31 @@
+import os
+# limpar o terminal antes de executar o programa
+os.system("cls")
+
+# execultar o programa
+print("Hello World")
+print("Hello, Python!")
+
+"""
+Identificadores python
+    É um nome usado para identificar uma variável
+    Podem conter: 
+        - A a Z ou a a z ou um sublinhad_
+    Não pode conter
+        - caracteres de pontuação como @, $, %
+    Diferencia maiuscula de minuscula
+
+    > Nome de classes começam com uma letra maiúscula e o resto 
+    minuscula
+    > Iniciar um identificador com um único sublinhado inicial 
+    indica que o identificador é privado
+    > Iniciar um identificador com dois sublinhados iniciais indica
+    um identificador fortemente privado
+    > Se o identificar começar e terminar com dois sublinhados é um
+    nome especial da linguagem
+Palavras reservadas
+    and, assert, break, class, continue, def, del, elif, else, except,
+    exec, finally, for, from, global, if, import, in, is, lambda, not,
+    or, pass, print, raise, return, try, while, with, yield
+
+"""
